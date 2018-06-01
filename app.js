ea457ec57e7995bff09cf7905874d6db540813f2
Shenghui Xue <sxue41@gatech.edu>
2018-05-31 23:44:43 -0700

    1

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static('./build'));
 
 app.get('/', function (req, res) {
-  res.sendFile('./build/index.html');
+  res.sendFile(__dirname +'/build/index.html');
 });
 
 app.get('/test', function(req, res) {
