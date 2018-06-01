2964773e3f2d7da0212d607cd2a36ae18b8d93a3
Shenghui Xue <sxue41@gatech.edu>
2018-05-31 23:55:35 -0700

    edit express

--- app.js
+++ app.js
@@ -2,19 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('./build'));
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname +'/build/index.html');
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
-app.get('/test', function(req, res) {
-	res.send("you hit the testing route");
-}) 
-
-app.get('/*', function(req, res) {
-	res.sendFile(__dirname +'/build/index.html');
-}) 
 
 //app.listen(9000);
 app.listen (process.env.PORT, process.env.IP, function () {
